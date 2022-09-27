577d1ff0d6dc0a56d04abdd06367f2b0c40081d4
Julian Gallegos <julianjgallegos1@gmail.com>
2022-09-27 19:45:56 +0000

    small style edits

--- src/components/CityForm.js
+++ src/components/CityForm.js
@@ -5,9 +5,9 @@ import Button from "react-bootstrap/Button";
 class CityForm extends React.Component {
     render() {
         return (
-            <Form onSubmit={this.props.handleSubmitForm}>
+            <Form className="my-4 mx-5" onSubmit={this.props.handleSubmitForm}>
                 <Form.Group className="mb-3" controlId="form">
-                    <Form.Label>City Name</Form.Label>
+                    <Form.Label>Try "Seattle"!</Form.Label>
                     <Form.Control placeholder="Enter name of a city" onChange={this.props.handleChangeForm}/>
                 </Form.Group>
                 <Button variant="primary" type="submit">
